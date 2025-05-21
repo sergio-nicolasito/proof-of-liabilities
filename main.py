d77f882975f724de97e81bdf3ddbec5910eccc2c
sergio-nicolasito <simplementenicolai@gmail.com>
2025-05-21 04:33:57 -0300

    Update main.py
    e8795220f3dd9c395f3ef3bd5485e087d0800a7805e2449058be6a2ee40d84fa

--- main.py
+++ main.py
@@ -1,77 +1,77 @@
-import csv
-from lib.merkle import MerkleSumTree, ProofStep, Leaf, Node, verify_merkle_proof
-import argparse
+importación CSV
+ lib.mepapel   mepapel importación MerkleSumTree, ProofStep, Leaf, Nodo, verifica_merkle_proof 
+ importación    argparse   
 
-def get_merkle_proofs(tree: MerkleSumTree, user_ids: list[str]) -> dict[str, list[ProofStep]]:
-    proofs = {}
+def..... get_merkle_proofs(árbol: MerkleSumTree, user_ids: lista[STr]) -> Dic.[Str, lista.[Prueba de paso]]:
+  Pruebas =  {}
 
-    for id in user_ids:
-        proof = tree.get_proof(id)
-        proofs[id] = proof
+     para ID en User_ids: 
+  Prueba = árbol.  get_proof(ID)
+  pruebas  [ID] = prueba
 
-    return proofs
+     volver pruebas 
 
-def verify_merkle_proofs(user_balances: list[Leaf], proofs: dict[str, list[ProofStep]], root_node: Node, audit_id: str):
-    for user_balance in user_balances:
-        verify_merkle_proof(root_node, proofs[user_balance.id], audit_id, user_balance)
+def... verifique_merkle_proofs(User_Balances: Lista[Hoja], pruebas: Dic.[Str, lista.[Prueba de paso]], root_node: Nodo, audit_id: str):
+     para user_balance en   user_balances:  
+        verifique_merkle_proof(root_node, pruebas.[user_balance.ID], audit_id, user_balance)
 
-def parse_arguments():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-i', '--input', help='Relative path to the input file', required = True)
-    parser.add_argument('-o','--output',  nargs='+', help='Relative path for the output files. First path refers to the tree output, second path refers to the proofs output', required=True)
-    parser.add_argument('-a','--audit_id',  help='Audit ID for this PoL audit')
+def... parse_argumentos():
+      parser = argparse.  ArgumentParser()
+ parser. add_argumento("-i", '--entrada', ayuda='Ruta relativa al archivo de entrada', requerido =  True)
+ parser. add_argumento('-o','-salida', nargs='+', ayuda='Ruta relativa para los archivos de salida. La primera ruta se refiere a la salida del árbol, la segunda ruta se refiere a la salida de pruebas., requerido =True)
+ parser. add_argumento('-a','-audito_id', ayuda='Identificador de auditoría para esta auditoría de PoL')
 
-    args = parser.parse_args()
+ ARGS = analizador. parse_args()
     
-    return args.input, args.output, args.audit_id
+     volver args. entrada, args.salida, args.audit_id
 
-def decode_user_balance(user_balance: tuple[str, str]) -> Leaf:
-    balance_str: str = user_balance[1]
-    new_balance_list: list[tuple[str, str]] = []
+def..... decode_user_balance(User_Balance: Tupla[Str, Str.]) -> Hoja:
+ Balance_Str: St = User_Balance [1]
+ new_balance_list: lista [Tupla[Str, Str.]] = []
 
-    balance_list_splitted = balance_str.split('|')
-    for balance in balance_list_splitted:
-        splitted_balance = balance.split(':')
-        if float(splitted_balance[1]) < 0: raise Exception("User balance must be positive")
+ balance_list_splitted = balance_str. dividir('|')
+     para equilibrio en balance_list_splitted: 
+ splitted_balance = balance. dividir(':')
+ Si flotar(splitted_balance[1]) < 0: elevar Excepción("El equilibrio del usuario debe ser positivo")
 
-        new_balance_list.append((splitted_balance[0], splitted_balance[1]))
+ new_balance_list. anexar((splitted_balance[0], splitted_balance[1]))
 
-    filtered_balance_list = list(filter(lambda balance: not float(balance[1]) == 0, new_balance_list))
-    return Leaf(user_balance[0], { balance[0]: balance[1] for balance in filtered_balance_list })
+ filtered_balance_list = lista(filtro(  equilibrio:  (equilibrio[1]) == 0, new_balance_list))
+     volver  Hoja(user_balance[0], {  equilibrio[0]: equilibrio[1] Para equilibrio en filtered_balance_list. })
 
-if __name__ == '__main__':
-    input_path, output_paths, audit_id = parse_arguments()
+Si __name__ == '__main__':
+ input_path, output_paths, audit_id = parse_argumentos()
 
-    with open(input_path, 'r+') as file:
-        reader = csv.DictReader(file)
+ Con abierto(input_path, 'R+')  como Archivo:
+ Lector = CSV. Lector directo(archivo)
 
-        encoded_user_balances: list[tuple[str, str]] = [(row['id'].strip(), row['balances'].strip()) for row in reader]
-        user_balances: list[Leaf] = list(map(lambda user_balance: decode_user_balance(user_balance), encoded_user_balances))
+   Encoded_user_balances: lista   [Tupla[Str, Str.]] = [(Fila['Id'].Tira(), fila['balances'].strip()) for Fila in  lector]
+   User_Balances: Lista   [Hoja] = lista(mapa(Lambda User_Balance: (user_balance), encoded_user_balances))
 
-        mst = MerkleSumTree(user_balances, hash_type = 'sha256', salt = audit_id.strip(), shuffle = True)
+ MST = MerkleSumTree(user_balances, hash_type = 'sha256', salt = audit_id.strip(), shuffle = True)
 
-        proofs = get_merkle_proofs(mst, list(map(lambda ub: ub.id, user_balances)))
+ Pruebas = get_merkle_proofs(mst, list(map(lambda ub: ub.id, user_balances)))
 
         verify_merkle_proofs(
             user_balances,
             proofs,
-            mst.get_root(), 
-            audit_id.strip()
+ Mst. get_root(), 
+ audit_id. strip()
         )
     
         with open(output_paths[0], 'w', newline='', encoding='utf-8') as write_file:
-            writer = csv.writer(write_file)
-            nodes = mst.get_nodes()
+ Escritor = CSV. writer(write_file)
+ nodos = MST. get_nodes()
 
             for node in nodes:
-                writer.writerow(node.to_string().split(','))
+ escritor. writerow(node.to_string().split(','))
 
         with open(output_paths[1], 'w', newline='', encoding='utf-8') as write_file:
-            writer = csv.writer(write_file)
+ Escritor = CSV. writer(write_file)
 
             for proof in proofs.items():
-                proof_map = map(lambda step: f"{{{step.to_string()}}}", proof[1])
-                merkle_proof = f"[{','.join(proof_map)}]"
+ proof_map = map(lambda step: f"{{{step.to_string()}}}", proof[1])
+ merkle_proof = f"[{','.join(proof_map)}]"
 
                 writer.writerow([proof[0], merkle_proof])
                 
